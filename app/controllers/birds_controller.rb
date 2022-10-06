5f4e5b01d8d02a4ba2470c8500af23a3f517fc7a
michael kaburu <michael.kaburu@student.moringaschool.com>
2022-10-06 21:53:47 +0300

    rails api deployment

---
+++ app/controllers/birds_controller.rb
@@ -0,0 +1,16 @@
+class BirdsController < ApplicationController
+    # GET /birds
+  def index
+    birds = Bird.all
+    render json: birds
+  end
+
+  # GET /birds/id
+  def show
+    bird = Bird.find(params[:id])
+    render json: bird
+  rescue ActiveRecord::RecordNotFound
+    render json: "Bird not found", status: :not_found
+  end
+
+end
